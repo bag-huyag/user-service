64d0894e04e0aa00b4e7e0109f502c90fc661e89
bag-huyag <64858585+bag-huyag@users.noreply.github.com>
2025-06-20 02:38:08 +0300

    Update handler user.go

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/bag-huyag/user-service/internal/kafka"
 	pb "github.com/bag-huyag/user-service/proto/gen"
@@ -35,4 +37,40 @@ func (h *UserHandler) CreateUser(ctx context.Context, in *pb.NewUser) (*pb.User,
 	return user, nil
 }
 
-//TODO Другие методы (Update, Delete...)
+func (h *UserHandler) UpdateUser(ctx context.Context, in *pb.User) (*pb.User, error) {
+	if in.Id == "" {
+		return nil, errors.New("missing user ID")
+	}
+
+	h.producer.SendUserEvent(kafka.UserEvent{
+		ID:    in.Id,
+		Name:  in.Name,
+		Email: in.Email,
+		Type:  "updated",
+	})
+
+	return in, nil
+}
+
+func (h *UserHandler) DeleteUser(ctx context.Context, in *pb.UserId) (*pb.Empty, error) {
+	if in.Id == "" {
+		return nil, errors.New("missing user ID")
+	}
+
+	h.producer.SendUserEvent(kafka.UserEvent{
+		ID:   in.Id,
+		Type: "deleted",
+	})
+
+	return &pb.Empty{}, nil
+}
+
+// GetUser — временная заглушка
+func (h *UserHandler) GetUser(ctx context.Context, in *pb.UserId) (*pb.User, error) {
+	return nil, fmt.Errorf("GetUser not implemented in user-service")
+}
+
+// GetUsers — временная заглушка
+func (h *UserHandler) GetUsers(ctx context.Context, _ *pb.Empty) (*pb.UserList, error) {
+	return nil, fmt.Errorf("GetUsers not implemented in user-service")
+}
